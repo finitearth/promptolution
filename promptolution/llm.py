e85f09f06622dc60d8af510b367aaf1cb66d26be
mo374z <schlager.mo@t-online.de>
2024-08-13 19:40:47 +0200

    Implement and test API calls to models

--- promptolution/llm.py
+++ promptolution/llm.py
@@ -1,11 +1,32 @@
+from langchain_anthropic import ChatAnthropic
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
 import numpy as np
 
-class LLM:
-    def predict(self, prompt: str) -> str:
-        pass
+OPENAI_API_KEY = open("openaitoken.txt", "r").read()
+ANTHROPIC_API_KEY = open("anthropictoken.txt", "r").read()
+
+# possible model names we'll use here are:
+# gpt-4o-2024-05-13, gpt-3.5-turbo-0125
+# claude-3-opus-20240229, claude-3-haiku-20240307
+
+
+class LLM():
+    def __init__(self, model_id: str):
+        if "claude" in model_id:
+            self.model = ChatAnthropic(model=model_id, api_key=ANTHROPIC_API_KEY)
+        elif "gpt" in model_id:
+            # we may use the BatchAPI instead of the LangChain interface here so save some costs
+            self.model = ChatOpenAI(model=model_id, api_key=OPENAI_API_KEY)
+        else:
+            raise ValueError(f"Unknown model: {model_id}")
+    
+    def get_response(self, prompt: str):
+        return self.model.invoke([HumanMessage(content=prompt)]).content
+
 
 class DummyLLM(LLM):
-    def predict(self, prompt: str) -> str:
+    def get_response(self, prompt: str) -> str:
         r = np.random.rand()
         if r < 0.3:
             return f"Joooo wazzuppp <prompt>hier gehts los {r} </prompt>"
--- promptolution/predictor.py
+++ promptolution/predictor.py
@@ -1,37 +1,21 @@
-from typing import Optional
-from langchain_anthropic import AnthropicLLM
-from langchain_openai import ChatOpenAI
-from langchain_core.prompts import PromptTemplate
 import numpy as np
 
-# from promptolution.tasks import Task
-
-def get_predictor(name: str, task):
-    if name == "dummy":
-        return DummyPredictor(name)
-    if "gpt" in name or "claude" in name:
-        return Predictor(name, task)
-    return Predictor(name, task)
+from promptolution.llm import LLM
 
 
 class Predictor:
-    def __init__(self, model_id: str, task):
-        OPENAI_API_KEY = open("../openaitoken.txt", "r").read()
-        ANTHROPIC_API_KEY = open("../anthropictoken.txt", "r").read()
-        if "claude" in model_id:
-            self.model = AnthropicLLM(model=model_id, api_key=ANTHROPIC_API_KEY)
-        elif "gpt" in model_id:
-            self.model = ChatOpenAI(model=model_id, api_key=OPENAI_API_KEY)
-        else:
-            raise ValueError(f"Unknown model: {model_id}")
+    def __init__(self, model_id: str):
+        self.llm = LLM(model_id)
 
     def predict(
         self,
-        prompt: Optional[str],
+        prompt: str,
         xs: np.ndarray,
-    ):
-        chain = PromptTemplate.from_template(prompt) | self.model
-        return chain.invoke({"question": str(xs)})
+    ) -> np.ndarray:
+        response = []
+        for x in xs:
+            response += self.llm.get_response(prompt + str(x))  
+        return np.array([response])
 
 
 class DummyPredictor(Predictor):
