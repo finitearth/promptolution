076a92ad9e6bd3c71f42f84269deb671dffa3f0a
Tom Zehle <t.zehle@gmail.com>
2024-10-03 00:46:29 +0200

    Update promptolution/prompt_creation.py
    Co-authored-by: Timo Heiß <87521684+timo282@users.noreply.github.com>

--- promptolution/prompt_creation.py
+++ promptolution/prompt_creation.py
@@ -37,7 +37,7 @@ def create_prompts_from_samples(task: BaseTask, llm: BaseLLM, n_samples: int = 3
     the paper Zhou et al. (2021) https://arxiv.org/pdf/2211.01910
     Samples are selected, such that 
     (1) all possible classes are represented
-    (2) the samples are representative as possible
+    (2) the samples are as representative as possible
 
     Args:
         task (BaseTask): The task to generate prompts for. Xs and Ys from this object are used to generate the prompts.
