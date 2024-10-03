ae5eb4dcef7beb63875f982a909fca0aaf160ea6
Timo Heiß <87521684+timo282@users.noreply.github.com>
2024-10-03 23:02:36 +0200

    Feature/workflows (#8)
    * chore: add codeowners file
    
    * chore: add python poetry action and docs workflow
    
    * chore: update pre-commit file
    
    * chore: update docs
    
    * chore: update logo
    
    * chore: add cicd pipeline for automated deployment
    
    * chore: update poetry version
    
    * chore: fix action versioning
    
    * chore: add gitattributes to ignore line count in jupyter notebooks
    
    * chore: add and update docstrings
    
    * chore: fix end of files
    
    * chore: update action versions
    
    * Update README.md
    
    ---------
    
    Co-authored-by: mo374z <schlager.mo@t-online.de>

--- promptolution/__init__.py
+++ promptolution/__init__.py
@@ -1,7 +1,3 @@
-from . import llms
-from . import optimizers
-from . import predictors
-from . import tasks
-from . import callbacks
-from . import config
-from .utils import prompt_creation
+"""Promptolution: A library for prompt tuning."""
+
+from . import callbacks, config, llms, optimizers, predictors, tasks
