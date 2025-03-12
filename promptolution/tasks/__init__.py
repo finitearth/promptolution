aa62c0d7e3d1c2397b23ef5b80c78ec267f88a61
finitearth <t.zehle@gmail.com>
2025-03-12 13:54:35 +0100

    bring back init

---
+++ promptolution/tasks/__init__.py
@@ -0,0 +1,3 @@
+"""Module for task-related functions and classes."""
+from promptolution.tasks.base_task import BaseTask, DummyTask
+from promptolution.tasks.classification_tasks import ClassificationTask
